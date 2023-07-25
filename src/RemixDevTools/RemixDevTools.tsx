28fc3ffeb9310ea35a8222527439f77077efc595
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-07-25 23:22:43 +0100

    revamp: Updating UI for RDT

--- src/RemixDevTools/RemixDevTools.tsx
+++ src/RemixDevTools/RemixDevTools.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { Logo } from "./components/Logo";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { RDTContextProvider } from "./context/RDTContext";
 import { tabs } from "./tabs";
 import { useTimelineHandler } from "./hooks/useTimelineHandler";
@@ -19,10 +19,14 @@ interface Props extends RemixDevToolsProps {
 
 const RemixDevTools = ({ defaultOpen, position }: Props) => {
   const [isOpen, setIsOpen] = useState(defaultOpen);
+
   const { activeTab, setActiveTab, setShouldConnectWithForge, height } =
     useRDTContext();
+
   const { enableResize, disableResize, isResizing } = useResize();
+
   useTimelineHandler();
+
   const { isConnected, isConnecting } = useGetSocket();
   const Component = tabs.find((tab) => tab.id === activeTab)?.component;
   const leftSideOriented =
@@ -42,22 +46,23 @@ const RemixDevTools = ({ defaultOpen, position }: Props) => {
           position === "top-right" && "rdt-right-0 rdt-top-0",
           position === "top-left" && "rdt-left-0 rdt-top-0",
           position === "middle-right" &&
-            "rdt-right-0 rdt-top-1/2 -rdt-translate-y-1/2",
+          "rdt-right-0 rdt-top-1/2 -rdt-translate-y-1/2",
           position === "middle-left" &&
-            "rdt-left-0 rdt-top-1/2 -rdt-translate-y-1/2"
+          "rdt-left-0 rdt-top-1/2 -rdt-translate-y-1/2",
+          isOpen && "rdt-hidden" // Hide the button when the dev tools is open
         )}
       >
         <Logo
           className={clsx(
             "rdt-h-14 rdt-w-14 rdt-rounded-full rdt-transition-all rdt-duration-200",
-            "rdt-hover:cursor-pointer rdt-hover:ring-2 rdt-ring-slate-600"
+            "rdt-hover:cursor-pointer rdt-hover:ring-2 rdt-ring-slate-600",
           )}
         />
       </div>
       <div
         style={{ zIndex: 9998, height }}
         className={clsx(
-          "rdt-duration-600 rdt-fixed rdt-bottom-0 rdt-left-0 rdt-box-border rdt-flex rdt-w-screen rdt-resize-y rdt-flex-col rdt-overflow-auto rdt-bg-[#212121] rdt-text-white rdt-opacity-0 rdt-transition-all",
+          "remix-dev-tools-window rdt-duration-600 rdt-fixed rdt-bottom-0 rdt-left-0 rdt-box-border rdt-flex rdt-w-screen rdt-resize-y rdt-flex-col rdt-overflow-auto rdt-bg-[#212121] rdt-text-white rdt-opacity-0 rdt-transition-all",
           isOpen ? "rdt-opacity-100 rdt-drop-shadow-2xl" : "rdt-h-0",
           isResizing ? "rdt-pointer-events-none" : "",
           isResizing && "rdt-cursor-grabbing "
@@ -71,41 +76,46 @@ const RemixDevTools = ({ defaultOpen, position }: Props) => {
             isResizing && "rdt-cursor-grabbing "
           )}
         />
-        <div className="rdt-flex rdt-h-8 rdt-w-full rdt-bg-gray-800">
-          {tabs
-            .filter(
-              (tab) =>
-                !(!isConnected && tab.requiresForge) && tab.id !== "timeline"
-            )
-            .map((tab) => (
+        <div className="rdt-h-8 rdt-w-full rdt-relative rdt-bg-gray-800 rdt-select-none">
+          <div className="rdt-overflow-y-auto rdt-w-full rdt-flex rdt-h-full rdt-pr-12">
+            {tabs
+              .filter(
+                (tab) =>
+                  !(!isConnected && tab.requiresForge) && tab.id !== "timeline"
+              )
+              .map((tab) => (
+                <div
+                  key={tab.id}
+                  onClick={() => setActiveTab(tab.id)}
+                  className={clsx(
+                    "rdt-flex rdt-cursor-pointer rdt-items-center rdt-gap-2 rdt-border-0 rdt-border-b rdt-border-r-2 rdt-border-solid rdt-border-b-[#212121] rdt-border-r-[#212121] rdt-px-4 rdt-font-sans rdt-transition-all rdt-duration-300",
+                    activeTab !== tab.id && "rdt-hover:opacity-50",
+                    activeTab === tab.id && "rdt-bg-[#212121]"
+                  )}
+                >
+                  {tab.icon} {tab.name}
+                </div>
+              ))}
+            {(!isConnected || isConnecting) && (
               <div
-                key={tab.id}
-                onClick={() => setActiveTab(tab.id)}
+                onClick={() => setShouldConnectWithForge(true)}
                 className={clsx(
-                  "rdt-flex rdt-cursor-pointer rdt-items-center rdt-gap-2 rdt-border-0 rdt-border-b rdt-border-r-2 rdt-border-solid rdt-border-b-[#212121] rdt-border-r-[#212121] rdt-px-4 rdt-font-sans rdt-transition-all rdt-duration-300",
-                  activeTab !== tab.id && "rdt-hover:opacity-50",
-                  activeTab === tab.id && "rdt-bg-[#212121]"
+                  isConnecting
+                    ? "rdt-pointer-events-none rdt-animate-pulse rdt-cursor-default"
+                    : "",
+                  "rdt-flex rdt-cursor-pointer rdt-items-center rdt-gap-2 rdt-border-0 rdt-border-b rdt-border-r-2 rdt-border-solid rdt-border-b-[#212121] rdt-border-r-[#212121] rdt-px-4 rdt-font-sans rdt-transition-all"
                 )}
               >
-                {tab.icon} {tab.name}
+                <Radio size={16} />
+                {isConnecting
+                  ? "Connecting to Forge..."
+                  : "Connect to Remix Forge"}
               </div>
-            ))}
-          {(!isConnected || isConnecting) && (
-            <div
-              onClick={() => setShouldConnectWithForge(true)}
-              className={clsx(
-                isConnecting
-                  ? "rdt-pointer-events-none rdt-animate-pulse rdt-cursor-default"
-                  : "",
-                "rdt-flex rdt-cursor-pointer rdt-items-center rdt-gap-2 rdt-border-0 rdt-border-b rdt-border-r-2 rdt-border-solid rdt-border-b-[#212121] rdt-border-r-[#212121] rdt-px-4 rdt-font-sans rdt-transition-all"
-              )}
-            >
-              <Radio size={16} />
-              {isConnecting
-                ? "Connecting to Forge..."
-                : "Connect to Remix Forge"}
-            </div>
-          )}
+            )}
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg" onClick={() => setIsOpen(false)} fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="rdt-w-6 rdt-h-6 rdt-absolute rdt-right-4 rdt-cursor-pointer rdt-top-1/2 -rdt-translate-y-1/2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
         </div>
         <div className="rdt-flex rdt-h-full rdt-w-full rdt-overflow-y-hidden">
           <div
@@ -120,7 +130,7 @@ const RemixDevTools = ({ defaultOpen, position }: Props) => {
           <div
             className={clsx(
               "rdt-z-10 rdt-h-full rdt-w-2/3 rdt-p-2",
-              leftSideOriented ? "rdt-pl-16" : "rdt-pr-16"
+              // leftSideOriented ? "rdt-pl-16" : "rdt-pr-16"
             )}
           >
             {tabs.find((t) => t.id === "timeline")?.component}
@@ -145,21 +155,35 @@ function useHydrated() {
 }
 
 interface RemixDevToolsProps {
-  // A port to connect to the Remix Forge in your vscode extension
+  /**
+   *  A port to connect to the Remix Forge in your vscode extension
+   *  @default 3003
+   */
   port?: number;
-  // Whether the dev tools should be open by default
+  /**
+   * Whether the dev tools should be open by default
+   * @default false
+   */
   defaultOpen?: boolean;
-  // Whether the dev tools require a url flag to be shown
+  /**
+   * Whether the dev tools requires a url flag to be shown
+   */
   requireUrlFlag?: boolean;
-  // Set the position of the trigger button
+  /**
+   * Where the dev tools should be positioned
+   * @default "bottom-right"
+   */
   position?:
-    | "bottom-right"
-    | "bottom-left"
-    | "top-right"
-    | "top-left"
-    | "middle-right"
-    | "middle-left";
-  // Show route boundaries when you hover over a route in active page tab
+  | "bottom-right"
+  | "bottom-left"
+  | "top-right"
+  | "top-left"
+  | "middle-right"
+  | "middle-left";
+  /**
+   * Whether the dev tools should show route boundaries
+   * @default false
+   */
   showRouteBoundaries?: boolean;
 }
 
@@ -176,7 +200,9 @@ const RDTWithContext = ({
   useOutletAugment(showRouteBoundaries);
 
   if (!hydrated || !isDevelopment) return null;
+
   if (requireUrlFlag && !url.includes("rdt=true")) return null;
+
   return (
     <RDTContextProvider showRouteBoundaries={showRouteBoundaries} port={port}>
       <RemixDevTools position={position} defaultOpen={defaultOpen} />
