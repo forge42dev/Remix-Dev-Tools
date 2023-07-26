263e9e832f92d65e7aa839d491c90d62f0e391b8
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2023-07-26 13:53:27 +0100

    chore: Added horizontal scroll hook

---
+++ src/RemixDevTools/hooks/useHorizontalScroll.ts
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Provide horizontal scroll capabilities to a component.
+ */
+
+import { useRef, useEffect } from "react"
+
+export default function useHorizontalScroll() {
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const elem = ref.current
+    const onWheel = (ev: WheelEvent) => {
+      if (!elem || ev.deltaY === 0) return;
+
+      elem.scrollTo({
+        left: elem.scrollLeft + ev.deltaY,
+        behavior: 'smooth'
+      })
+    }
+
+    elem && elem.addEventListener('wheel', onWheel, { passive: true })
+
+    return () => {
+      elem && elem.removeEventListener('wheel', onWheel)
+    }
+  }, [])
+
+  return ref;
+
+}
