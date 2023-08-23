8693fa5a0f2ad71816c5fdd599ea30dbf5ad05c6
Alem Tuzlak <t.zlak@hotmail.com>
2023-08-23 11:41:09 +0200

    v2.1.4 - Bug fix for route boundaries

--- src/RemixDevTools/hooks/useOutletAugment.tsx
+++ src/RemixDevTools/hooks/useOutletAugment.tsx
@@ -1,24 +1,30 @@
 import { useEffect } from "react";
 import { InvisibleBoundary } from "../init/project";
 
-const isHooked = Symbol("isHooked");
+const isHooked = Symbol("isHooked") as any;
+
 export function useOutletAugment() {
   useEffect(() => {
-    if (window.__remixRouteModules[isHooked as any]) return;
+    if (window.__remixRouteModules[isHooked]) return;
 
     window.__remixRouteModules = new Proxy(window.__remixRouteModules, {
       get: function (target, property) {
-        if (property === isHooked) return target[property as any];
+        const key = property as any;
+        if (property === isHooked) return target[key];
         if (property === "root") return target[property];
-        const value = target[property as any];
+        const value = target[key];
 
         if (value?.default && value.default.name !== "hooked") {
           return {
             ...value,
             default: function hooked() {
+              // Does not pollute the DOM with invisible boundaries after the first render
+              if (document.getElementsByClassName(key).length) {
+                return <value.default />;
+              }
               return (
                 <>
-                  <InvisibleBoundary path={property as any} />
+                  <InvisibleBoundary path={key} />
                   <value.default />
                 </>
               );
