56ae0b92d8012a69c96bb0b0c76ba91b991ab401
Alem Tuzlak <t.zlak@hotmail.com>
2024-03-10 16:19:43 +0100

    fix

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,11 +1,15 @@
 import { unstable_RemixPWA } from '@remix-pwa/dev'
-import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
-import { remixDevTools } from 'remix-development-tools/vite'
+import { remixDevTools } from 'remix-development-tools'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths(), remixDevTools(), unstable_RemixPWA()],
+  plugins: [
+    remixDevTools({ includeInProd: true, server: { silent: true } }),
+    remix(),
+    tsconfigPaths(),
+  ],
   server: {
     open: true,
     port: 3000,
