8665fc8db354fa4839428e49e95c238f9f979177
Alem Tuzlak <t.zlak@hotmail.com>
2024-08-21 16:39:56 +0200

    Fix issues with lighthouse in local dev

--- src/test-apps/remix-vite/vite.config.ts
+++ src/test-apps/remix-vite/vite.config.ts
@@ -2,21 +2,20 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { remixDevTools, defineRdtConfig } from "remix-development-tools"
-
+ 
 const config = defineRdtConfig({
   client: {
   defaultOpen: false,
   panelLocation: "top",
   position: "top-right",
-  requireUrlFlag: true
+  requireUrlFlag: false
   },server: {},
   pluginDir: "./plugins",
   includeInProd: true,
   unstable_console: true
 }, ); 
-
 export default defineConfig({
-  plugins: [remixDevTools(config),remix({ future: {
+  plugins: [remixDevTools( config),remix({ future: {
     unstable_singleFetch: true
   } }), tsconfigPaths()],
   server: {
